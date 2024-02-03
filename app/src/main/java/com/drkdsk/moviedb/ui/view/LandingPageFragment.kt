cba7f6199f29e064b0849b9e065072d074f73226
Alfredo Palacios <alfredopalacios@blue.gen.in>
2024-02-03 17:40:48 -0600

    navComponent implementado

---
+++ app/src/main/java/com/drkdsk/moviedb/ui/view/LandingPageFragment.kt
@@ -0,0 +1,25 @@
+package com.drkdsk.moviedb.ui.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.drkdsk.moviedb.databinding.FragmentLandingpageBinding
+
+class LandingPageFragment : Fragment() {
+
+    private var _binding: FragmentLandingpageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentLandingpageBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/drkdsk/moviedb/ui/view/MainActivity.kt
+++ app/src/main/java/com/drkdsk/moviedb/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.drkdsk.moviedb
+package com.drkdsk.moviedb.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.drkdsk.moviedb.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
